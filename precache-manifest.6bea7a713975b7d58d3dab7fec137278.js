7f00bc3b4b291962eac7014be8baef345bf2f96d
Md Dilshad Alam <mddalam1@users.noreply.github.com>
2020-06-20 17:11:09 +0530

    Updates

---
+++ precache-manifest.6bea7a713975b7d58d3dab7fec137278.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce5003706a5db46ca96dc65911ea5f34",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4e92c598ce93ec236b5d",
+    "url": "/monsters-rolodex/static/css/2.8df856cb.chunk.css"
+  },
+  {
+    "revision": "3711454dc592fdd281dc",
+    "url": "/monsters-rolodex/static/css/main.7bff675f.chunk.css"
+  },
+  {
+    "revision": "4e92c598ce93ec236b5d",
+    "url": "/monsters-rolodex/static/js/2.5abf65fa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.5abf65fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3711454dc592fdd281dc",
+    "url": "/monsters-rolodex/static/js/main.91039d57.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/monsters-rolodex/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/monsters-rolodex/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "4a5d4d855d11fae79cc864bdacb2f479",
+    "url": "/monsters-rolodex/static/media/fa-brands-400.4a5d4d85.eot"
+  },
+  {
+    "revision": "4c1da237bdae0773309df93b2cd48e09",
+    "url": "/monsters-rolodex/static/media/fa-brands-400.4c1da237.ttf"
+  },
+  {
+    "revision": "5734d789b25228cbafc64a58ae971aca",
+    "url": "/monsters-rolodex/static/media/fa-brands-400.5734d789.woff"
+  },
+  {
+    "revision": "778b1f251bea7412048da95b87bf816f",
+    "url": "/monsters-rolodex/static/media/fa-brands-400.778b1f25.svg"
+  },
+  {
+    "revision": "91a23e8bf2b4b84c39311cb5eb23aaa0",
+    "url": "/monsters-rolodex/static/media/fa-brands-400.91a23e8b.woff2"
+  },
+  {
+    "revision": "260be4f29c0b2ce47480afb23f38f237",
+    "url": "/monsters-rolodex/static/media/fa-regular-400.260be4f2.ttf"
+  },
+  {
+    "revision": "5c674c9216c06ede2f618aa58ae71116",
+    "url": "/monsters-rolodex/static/media/fa-regular-400.5c674c92.woff2"
+  },
+  {
+    "revision": "66578cdbb6dc01f527a53971051b3e85",
+    "url": "/monsters-rolodex/static/media/fa-regular-400.66578cdb.svg"
+  },
+  {
+    "revision": "6b20949b3a679c30d09f64acd5d3317d",
+    "url": "/monsters-rolodex/static/media/fa-regular-400.6b20949b.eot"
+  },
+  {
+    "revision": "d44ad00c44e46fd29f6126fa7d888cde",
+    "url": "/monsters-rolodex/static/media/fa-regular-400.d44ad00c.woff"
+  },
+  {
+    "revision": "412a43d6840addd683665ec12c30f810",
+    "url": "/monsters-rolodex/static/media/fa-solid-900.412a43d6.woff2"
+  },
+  {
+    "revision": "486853107489520b3265b19b191626f8",
+    "url": "/monsters-rolodex/static/media/fa-solid-900.48685310.svg"
+  },
+  {
+    "revision": "9a1672a8a8d91fbf82c71f451d495253",
+    "url": "/monsters-rolodex/static/media/fa-solid-900.9a1672a8.eot"
+  },
+  {
+    "revision": "c65d154888aa166982dac3e72e7380ec",
+    "url": "/monsters-rolodex/static/media/fa-solid-900.c65d1548.ttf"
+  },
+  {
+    "revision": "f3a7d3b5880544a91e9a7e6f8f35d4d2",
+    "url": "/monsters-rolodex/static/media/fa-solid-900.f3a7d3b5.woff"
+  }
+]);
